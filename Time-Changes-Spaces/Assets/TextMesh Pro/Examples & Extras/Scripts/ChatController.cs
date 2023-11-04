5d623208c26c64eb36ecd3283da9ffe8a10e1a61
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-11-04 12:43:14 +0300

    Created project

---
+++ Time-Changes-Spaces/Assets/TextMesh Pro/Examples & Extras/Scripts/ChatController.cs
@@ -0,0 +1,60 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatController : MonoBehaviour
+{
+    public TMP_InputField ChatInputField;
+
+    public TMP_Text ChatDisplayOutput;
+
+    public Scrollbar ChatScrollbar;
+
+    private void OnEnable()
+    {
+        ChatInputField.onSubmit.AddListener(AddToChatOutput);
+    }
+
+    private void OnDisable()
+    {
+        ChatInputField.onSubmit.RemoveListener(AddToChatOutput);
+    }
+
+    private void AddToChatOutput(string newText)
+    {
+        // Clear Input Field
+        ChatInputField.text = string.Empty;
+
+        System.DateTime timeNow = System.DateTime.Now;
+
+        string formattedInput =
+            "[<#FFFF80>"
+            + timeNow.Hour.ToString("d2")
+            + ":"
+            + timeNow.Minute.ToString("d2")
+            + ":"
+            + timeNow.Second.ToString("d2")
+            + "</color>] "
+            + newText;
+
+        if (ChatDisplayOutput != null)
+        {
+            // No special formatting for first entry
+            // Add line feed before each subsequent entries
+            if (ChatDisplayOutput.text == string.Empty)
+            {
+                ChatDisplayOutput.text = formattedInput;
+            }
+            else
+            {
+                ChatDisplayOutput.text += "\n" + formattedInput;
+            }
+        }
+
+        // Keep Chat input field active
+        ChatInputField.ActivateInputField();
+
+        // Set the scrollbar to the bottom when next text is submitted.
+        ChatScrollbar.value = 0;
+    }
+}
